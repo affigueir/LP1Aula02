ead961ed269b65faaf03d9a49bc76af6b571da17
André Furão Figueira <affigueir@gmail.com>
2020-03-05 10:39:18 +0000

    Add Cilindro

---
+++ Cilindro/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira a altura do cilindro:");
+            string h = Console.ReadLine();
+
+            Console.WriteLine("Insira o raio do cilindro:");
+            string r = Console.ReadLine();
+
+            float h1 = float.Parse(h);
+            float r1 = float.Parse(r);
+
+            float pi = 3.1415926f;
+
+            float V = pi * (r1 * r1) * h1;
+
+            Console.WriteLine($"{V}");
+
+
+
+        }
+    }
+}
