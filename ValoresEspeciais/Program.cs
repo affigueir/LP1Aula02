05d03f50cfed45e7a6113090517bf40f02971b61
André Furão Figueira <affigueir@gmail.com>
2020-03-05 12:03:41 +0000

    Over and Under

--- ValoresEspeciais/Program.cs
+++ ValoresEspeciais/Program.cs
@@ -6,32 +6,46 @@ namespace ValoresEspeciais
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("int values:");
-            Console.WriteLine($"{int.MinValue}");
-            Console.WriteLine($"{int.MaxValue}");
+            //Console.WriteLine("int values:");
+            //Console.WriteLine($"{int.MinValue}");
+            //Console.WriteLine($"{int.MaxValue}");
+            //
+            //Console.WriteLine("float values:");
+            //Console.WriteLine($"{float.MinValue}");
+            //Console.WriteLine($"{float.MaxValue}");
+            //
+            //Console.WriteLine("double values:");
+            //Console.WriteLine($"{double.MinValue}");
+            //Console.WriteLine($"{double.MaxValue}");
+            //
+            //Console.WriteLine("long values:");
+            //Console.WriteLine($"{long.MaxValue}");
+            //Console.WriteLine($"{long.MaxValue}");
+//
+ //
+            //Console.WriteLine("float Infinity:");
+            //Console.WriteLine($"{float.NegativeInfinity}");
+            //Console.WriteLine($"{float.PositiveInfinity}");
+            //
+            //Console.WriteLine("double Infinity:");
+            //Console.WriteLine($"{double.NegativeInfinity}");
+            //Console.WriteLine($"{double.PositiveInfinity}");
             
-            Console.WriteLine("float values:");
-            Console.WriteLine($"{float.MinValue}");
-            Console.WriteLine($"{float.MaxValue}");
-            
-            Console.WriteLine("double values:");
-            Console.WriteLine($"{double.MinValue}");
-            Console.WriteLine($"{double.MaxValue}");
-            
-            Console.WriteLine("long values:");
-            Console.WriteLine($"{long.MaxValue}");
-            Console.WriteLine($"{long.MaxValue}");
+            int ovint = int.MaxValue;
+            float ovfloat = float.MaxValue;
+            double ovdouble = double.MaxValue;
+            long ovlong = long.MaxValue;
 
- 
-            Console.WriteLine("float Infinity:");
-            Console.WriteLine($"{float.NegativeInfinity}");
-            Console.WriteLine($"{float.PositiveInfinity}");
-            
-            Console.WriteLine("double Infinity:");
-            Console.WriteLine($"{double.NegativeInfinity}");
-            Console.WriteLine($"{double.PositiveInfinity}");
-            
-            
+            float unfloat = 100000000000000;
+            double undouble = double.MaxValue;
+
+            Console.WriteLine(ovint + 1);
+            Console.WriteLine(ovfloat + 1);
+            Console.WriteLine(ovdouble + 1);
+            Console.WriteLine(ovlong + 1);
+            Console.WriteLine(unfloat + 0.00000001f);
+            Console.WriteLine(undouble * 2000000000);
+
             
             
          
