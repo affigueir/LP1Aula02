a2e067713f24a21e24a6b3443f5fa655dcd1b11b
André Furão Figueira <affigueir@gmail.com>
2020-03-05 11:40:35 +0000

    Values and Infinity

---
+++ ValoresEspeciais/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ValoresEspeciais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("int values:");
+            Console.WriteLine($"{int.MinValue}");
+            Console.WriteLine($"{int.MaxValue}");
+            
+            Console.WriteLine("float values:");
+            Console.WriteLine($"{float.MinValue}");
+            Console.WriteLine($"{float.MaxValue}");
+            
+            Console.WriteLine("double values:");
+            Console.WriteLine($"{double.MinValue}");
+            Console.WriteLine($"{double.MaxValue}");
+            
+            Console.WriteLine("long values:");
+            Console.WriteLine($"{long.MaxValue}");
+            Console.WriteLine($"{long.MaxValue}");
+
+ 
+            Console.WriteLine("float Infinity:");
+            Console.WriteLine($"{float.NegativeInfinity}");
+            Console.WriteLine($"{float.PositiveInfinity}");
+            
+            Console.WriteLine("double Infinity:");
+            Console.WriteLine($"{double.NegativeInfinity}");
+            Console.WriteLine($"{double.PositiveInfinity}");
+            
+            
+            
+            
+         
+
+        }
+    }
+}
