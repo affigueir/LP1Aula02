f9fed54e6aa3b4b439aa9add7ae24abf4d805a46
André Furão Figueira <affigueir@gmail.com>
2020-03-05 09:34:39 +0000

    Add IncDec

---
+++ IncDec/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace IncDec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 6;
+            int b = 5;
+
+            int c = a++ + ++b;
+
+            //Console.WriteLine(c);
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+
+            Console.WriteLine($"{a} {b} {c}");  
+
+        }
+    }
+}
